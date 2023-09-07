240ae6de35ca2e4347f631513a08e679d7183d81
David Angarita Rodriguez <119277268+D4vid-SN@users.noreply.github.com>
2023-09-07 08:39:14 -0500

    Add files via upload

---
+++ sueldoAnual.js
@@ -0,0 +1,6 @@
+let sueldo = prompt(parseInt('ingrese su sueldo'))
+let boniMensual = 200
+sueldoAnual = sueldo * 12
+boniMensualTotal = boniMensual * 12
+sueldoAnualTotal = boniMensualTotal + sueldoAnual
+console.log(`su sueldo anual es de ${sueldoAnualTotal}`);
