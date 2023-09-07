4824eaba08085e04fa48f3c208cd03379ff7991d
David Angarita Rodriguez <119277268+D4vid-SN@users.noreply.github.com>
2023-09-07 08:27:44 -0500

    Add files via upload

---
+++ promedioSueldos.js
@@ -0,0 +1,9 @@
+let sueldo = [100000, 51122, 22453655, 642548];
+let promedio = 0;
+
+for (let index = 0; index < sueldo.length; index++) {
+    sueldo+sueldo[index]
+    promedio = sueldo/sueldo[index]
+
+}
+console.log(promedio);
